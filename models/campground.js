d321051b93ffdb44cbeafe2404f27c2bdd8e030d
ZacharyMenefee <zcmenefee02@gmail.com>
2022-02-13 16:11:53 -0500

    Lots of refactoring, refactored into controllers, added star ratings, grouped middleware

--- models/campground.js
+++ models/campground.js
@@ -1,5 +1,6 @@
 
 const mongoose = require('mongoose');
+const { UserExistsError } = require('passport-local-mongoose/lib/errors');
 const Review = require('./review')
 const Schema = mongoose.Schema;
 
@@ -9,6 +10,10 @@ const CampgroundSchema = new Schema({
     price: Number,
     description: String,
     location: String,
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+    },
     reviews: [
         {
             type: Schema.Types.ObjectId,
--- seeds/index.js
+++ seeds/index.js
@@ -21,6 +21,7 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
+            author: '62096589f7e7cdc4b51881f1',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: 'https://source.unsplash.com/collection/483251',
