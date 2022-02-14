8deb852e1c8ad38665667c59aef87cb7efd5e0c2
ZacharyMenefee <zcmenefee02@gmail.com>
2022-02-13 20:28:50 -0500

    I got lazy with my commits.... I added image uploading, security features, moved to a prod db, added maps, fixed to controllers

--- models/campground.js
+++ models/campground.js
@@ -1,18 +1,38 @@
-
 const mongoose = require('mongoose');
-const { UserExistsError } = require('passport-local-mongoose/lib/errors');
 const Review = require('./review')
 const Schema = mongoose.Schema;
 
+const ImageSchema = new Schema({
+    url: String,
+    filename: String
+});
+
+ImageSchema.virtual('thumbnail').get(function () {
+    return this.url.replace('/upload', '/upload/w_200');
+});
+
+const opts = { toJSON: { virtuals: true } };
+
 const CampgroundSchema = new Schema({
-    title: String, 
-    image: String,
+    title: String,
+    images: [ImageSchema],
+    geometry: {
+        type: {
+            type: String,
+            enum: ['Point'],
+            required: true
+        },
+        coordinates: {
+            type: [Number],
+            required: true
+        }
+    },
     price: Number,
     description: String,
     location: String,
     author: {
         type: Schema.Types.ObjectId,
-        ref: 'User',
+        ref: 'User'
     },
     reviews: [
         {
@@ -20,6 +40,12 @@ const CampgroundSchema = new Schema({
             ref: 'Review'
         }
     ]
+}, opts);
+
+CampgroundSchema.virtual('properties.popUpMarkup').get(function () {
+    return `
+    <strong><a href="/campgrounds/${this._id}">${this.title}</a><strong>
+    <p>${this.description.substring(0, 20)}...</p>`
 });
 
 CampgroundSchema.post('findOneAndDelete', async function (doc) {
@@ -30,6 +56,6 @@ CampgroundSchema.post('findOneAndDelete', async function (doc) {
             }
         })
     }
-});
+})
 
 module.exports = mongoose.model('Campground', CampgroundSchema);
--- models/user.js
+++ models/user.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
-const passportLocalMongoose = require('passport-local-mongoose');
 const Schema = mongoose.Schema;
+const passportLocalMongoose = require('passport-local-mongoose');
 
 const UserSchema = new Schema({
     email: {
         type: String,
         required: true,
-        unique: true,
+        unique: true
     }
 });
 
